8fa35c7908457deeeb6f4e0286ad0d48b669825f
Sébastien Deleuze <sdeleuze@vmware.com>
2022-11-14 13:32:56 +0100

    Add HttpExchangeRepository bean and configure redirect for ALV compat

--- spring-cloud-serverless/src/main/java/com/example/helloapp/AlvCompatibilityConfiguration.java
+++ spring-cloud-serverless/src/main/java/com/example/helloapp/AlvCompatibilityConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.helloapp;
 
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -17,5 +20,13 @@ public class AlvCompatibilityConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/actuator/").setViewName("forward:/actuator");
+		registry.addViewController("/actuator/httptrace").setViewName("forward:/actuator/httpexchanges");
+		registry.addViewController("/actuator/httptrace/").setViewName("forward:/actuator/httpexchanges");
 	}
+
+	@Bean
+	public HttpExchangeRepository httpTraceRepository() {
+		return new InMemoryHttpExchangeRepository();
+	};
+
 }
